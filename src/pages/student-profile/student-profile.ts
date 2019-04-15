a4c6185102391e59fa26a105b50d801da2152190
JakobLopez <thejakoblopez@gmail.com>
2019-04-14 19:03:42 -0500

    quick link favorites list

--- src/pages/student-profile/student-profile.ts
+++ src/pages/student-profile/student-profile.ts
@@ -104,4 +104,16 @@ export class StudentProfilePage {
       console.log(e);
     }
   }
+
+    // Go to selected Teacher profile
+    viewUser(viewID:any) {
+      try {
+        this.navCtrl.push('ViewPage',{
+          item:viewID
+          });
+      }
+      catch (e) {
+        console.log(e);
+      }
+    }
 }
